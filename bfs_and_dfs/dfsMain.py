567232c7f8689033d647ea5595e980200db337d0
DamiaoFilho <damiaofilho0123@gmail.com>
2025-02-24 22:53:41 -0300

    :sparkles: feat: Displaying the graph

--- bfs_and_dfs/dfsMain.py
+++ bfs_and_dfs/dfsMain.py
@@ -7,6 +7,6 @@ if __name__ == "__main__":
     adj.display_graph()
 
     adj.dfs_search(adj.nodes[0], adj.nodes[10], 0, [adj.nodes[0]])
-    adj.visualize_paths()
     print(f"Best track: {[node.value for node in adj.best_track]}")
     print(f"Cost: {adj.max_cost}")
+    adj.visualize_paths()
