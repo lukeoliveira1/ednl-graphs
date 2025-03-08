38139014a3c3afb3a5af5ac25f83ecd091342c27
DamiaoFilho <damiaofilho0123@gmail.com>
2025-03-08 16:27:33 -0300

    :sparkles: tps finished

---
+++ tps/base.py
@@ -0,0 +1,65 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class Graph:
+    def __init__(self):
+        self.nodes = []
+
+    def print_heads(self):
+        for node in self.nodes:
+            print(node.value)
+
+    def insert(self, value):
+        if any(node.value == value for node in self.nodes):
+            return
+        self.nodes.append(Node(value))
+
+    def add_edge(self, v1, v2):
+        node1 = next((node for node in self.nodes if node.value == v1), None)
+        node2 = next((node for node in self.nodes if node.value == v2), None)
+
+        if node1 and node2:
+            last_node = node1
+            while last_node.next:
+                last_node = last_node.next
+            last_node.next = Node(v2)
+
+            last_node = node2
+            while last_node.next:
+                last_node = last_node.next
+            last_node.next = Node(v1)
+
+    def display_graph(self):
+        for node in self.nodes:
+            print(f"{node.value}", end="")
+            if node.next:
+                print(" --> ", end="")
+
+            current = node.next
+            while current:
+                if current.next:
+                    print(f"{current.value} --> ", end="")
+                else:
+                    print(current.value, end="")
+                current = current.next
+            print()
+
+if __name__ == "__main__":
+    adj = Graph()
+    adj.insert(1)
+    adj.insert(2)
+    adj.insert(3)
+    adj.insert(4)
+    adj.insert(5)
+
+    adj.add_edge(1, 2)
+    adj.add_edge(1, 3)
+    adj.add_edge(1, 4)
+    adj.add_edge(2, 4)
+    adj.add_edge(5, 4)
+    adj.add_edge(5, 2)
+
+    adj.display_graph()
---
+++ tps/main.py
@@ -0,0 +1,17 @@
+from tps import TPS 
+
+if __name__ == "__main__":
+    adj = TPS()
+    adj.load_from_csv("tps/graph.csv")
+    adj.display_graph()
+
+    adj.tps_dfs("V1")
+
+    print("Best track:")
+    for i, node in enumerate(adj.best_track):
+        if i == len(adj.best_track) - 1:
+            print(node.value)
+        else:
+            print(node.value, end=" -> ")
+
+    adj.visualize_tracks()
